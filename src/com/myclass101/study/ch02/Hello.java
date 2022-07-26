0226374e5f71cde1ba17438326082505c9585775
jinkyungkim12 <skwlsrud3@naver.com>
2022-07-26 15:41:09 +0900

    up

---
+++ src/com/myclass101/study/ch02/Hello.java
@@ -0,0 +1,16 @@
+package com.myclass101.study.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			//데이터타입 변수명 = 초기값;
+			//데이터타입 변수명
+			
+			boolean abc = true;
+			boolean xyz;
+			xyz = 10;
+	}
+
+}
