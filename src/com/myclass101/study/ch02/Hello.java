d38a1caf5a691aaf61c14e023e06f804e8d147c3
jinkyungkim12 <skwlsrud3@naver.com>
2022-07-26 16:22:43 +0900

    up

--- src/com/myclass101/study/ch02/Hello.java
+++ src/com/myclass101/study/ch02/Hello.java
@@ -10,7 +10,57 @@ public class Hello {
 			
 			boolean abc = true;
 			boolean xyz;
-			xyz = 10;
+			xyz = false;
+			
+			int width = 105;
+			String name = "김진경";
+			
+			// 상세 화면
+			String category = "굿즈 이모티콘 유랑";
+			String title = "[혜택연장] 무명 이모티콘 작가가 인기 작가가 된 비결 ~~";
+			int discountRate = 32;
+			int price = 43200;
+			int dicountPrice = 103400;
+			String classInfo ="클래스 정보";
+			int classAmount = 12;
+			boolean available = true;
+			boolean subtitle = true;
+			int reviewAmount = 66;
+			double reviewStar = 4.9;
+			String Id ="곰돌이";
+			String content ="유랑작가님 ~~";
+			int comment =2;
+			String curriculumSubtitle = "welcome 승인되는 이모티콘과 함께 만들어봐요!";
+			String curriculumList = "안녕하세요, 이모티콘 작가 유랑입니다.";
+			String refund ="환불정책";
+			
+			
+			System.out.println(name);
+			System.out.println("name 변수에 들어가 있는 값은: " + name);
+			System.out.println("내 이름은: " + name);
+			
+			
+			// 상세 화면 TEST
+			
+			System.out.println("카테고리: " + category);
+			System.out.println("강의 제목: " + title);
+			System.out.println("할인률: " + discountRate);
+			System.out.println("가격: " + price);
+			System.out.println("총 할인액: " + dicountPrice);
+			System.out.println("클래스 정보: " + classInfo);
+			System.out.println("클래스 분량: " + classAmount);
+			System.out.println("수강 가능일: " + available);
+			System.out.println("자막 포함 여부: " + subtitle);
+			System.out.println("리뷰 수: " + reviewAmount);
+			System.out.println("리뷰 별점: " + reviewStar);
+			System.out.println("리뷰 작성자 아이디: " + Id);
+			System.out.println("리뷰 내용: " + content);
+			System.out.println("댓글: " + comment);
+			System.out.println("커리큘럼 목차 소제목: " + curriculumSubtitle);
+			System.out.println("커리큘럼 목차: " + curriculumList);
+			System.out.println("환불 정책: " + refund);
+			
+			
 	}
 
 }
