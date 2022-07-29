cadb3823f5695d9b17d4f737b67d9a00a80eb35b
jinkyungkim12 <skwlsrud3@naver.com>
2022-07-29 15:53:20 +0900

    up

---
+++ src/com/myclass101/study/ch02/LongExample.java
@@ -0,0 +1,20 @@
+package com.myclass101.study.ch02;
+
+public class LongExample {
+
+	public static void main(String[] args) {
+
+			
+		long var1 = 10;
+		long var2 = 20;
+		
+		
+		
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		
+		
+	}
+
+}
