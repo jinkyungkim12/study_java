58f1631521eb52504bc7ad7c3f16987cf380e1f6
jinkyungkim12 <skwlsrud3@naver.com>
2022-07-29 16:12:07 +0900

    up

---
+++ src/com/myclass101/study/ch02/BooleanExample.java
@@ -0,0 +1,18 @@
+package com.myclass101.study.ch02;
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+
+		boolean stop = false; //true, false 값만 들어갈 수 있다.
+		
+		if(stop) {
+			System.out.println("중지합니다.");
+		}
+		else {
+			System.out.println("시작합니다.");
+		}
+		
+	}
+
+}
