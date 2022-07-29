1c2e940dd15318a96856abfc8caf0cda090908f3
jinkyungkim12 <skwlsrud3@naver.com>
2022-07-29 16:04:08 +0900

    up

---
+++ src/com/myclass101/study/ch02/FloatDoubleExample.java
@@ -0,0 +1,19 @@
+package com.myclass101.study.ch02;
+
+public class FloatDoubleExample {
+
+	public static void main(String[] args) {
+
+		
+			double var1 = 3.14; //정확도 높음. 데이터 용량을 더 많이 차지 함. 사용 권장.
+			float var2 = 3.14f;
+			float var3 = 3.14F;
+			
+			System.out.println(var1);
+			System.out.println(var2);
+			System.out.println(var3);
+			
+			
+	}
+
+}
--- src/com/myclass101/study/ch02/LongExample.java
+++ src/com/myclass101/study/ch02/LongExample.java
@@ -8,13 +8,13 @@ public class LongExample {
 		long var1 = 10;
 		long var2 = 2045646443L;
 		
-		// long var2 = 100000000000;
+		// long var3 = 1000000000000;
 		
 		
 		
 		System.out.println(var1);
 		System.out.println(var2);
-		
+		// System.out.println(var3);
 		
 	}
 
