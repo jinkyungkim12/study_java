b4bc8a27e4af651c988a821b7f08667287298818
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-02 14:13:14 +0900

    up

--- src/com/myclass101/study/ch02/OperationsPromotionExample.java
+++ src/com/myclass101/study/ch02/OperationsPromotionExample.java
@@ -4,8 +4,58 @@ public class OperationsPromotionExample {
 
 	public static void main(String[] args) {
 
+		//byte 변수 byteValue 선언하고 12로 초기화
+		//byte 변수 byteValue2 선언하고 12로 초기화
+		//byte 변수 byteValue3 선언하고 위 두 변수를 합 연산 수형
+		// byteValue3 결과 출력
 		
 		
+		byte byteValue = 12;
+		
+
+		byte byteValue2 = 12;
+	
+		//byte byteValue3 =  byteValue + byteValue2;
+		int intValue =  byteValue + byteValue2;
+		
+		System.out.println(intValue);
+		
+		
+		//short
+		
+		
+		short shortValue = 12;
+		short shortValue2 = 12;
+		int shortValue3 = shortValue + shortValue2;
+		System.out.println(shortValue3);
+		
+		//int
+		
+		int intValue2 = 12;
+		int intValue4 = 12;
+		long intValue3 = intValue2 + intValue4;
+		System.out.println(intValue3);
+		
+		//long
+		
+		long longValue = 12;
+		long longValue2 = 12;
+		long longValue3 = longValue + longValue2;
+		System.out.println(longValue3);
+		
+		//float
+		
+		float floatValue = 12;
+		float floatValue2 = 12;
+		double floatValue3 = floatValue + floatValue2;
+		System.out.println(floatValue3);
+		
+		//double
+		
+		double doubleValue = 12;
+		double doubleValue2 = 12;
+		double doubleValue3 = doubleValue + doubleValue2;
+		System.out.println(doubleValue3);
 	}
 
 }
