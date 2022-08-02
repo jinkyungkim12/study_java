183a22acc2a8c3ffb03f048154f2c4153c0fe78b
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-02 16:07:39 +0900

    up

---
+++ src/com/myclass101/study/ch03/ArithmeticOperatorExample.java
@@ -0,0 +1,37 @@
+package com.myclass101.study.ch03;
+
+public class ArithmeticOperatorExample {
+
+	public static void main(String[] args) {
+
+		
+//		int 변수 v1에 초기값 5
+//		int 변수 v2에 초기값 2
+		
+//		int 변수 result1 에 + 연산 후 출력
+//		int 변수 result2 에 - 연산 후 출력
+//		int 변수 result3 에 * 연산 후 출력
+//		int 변수 result4 에 / 연산 후 출력
+//		int 변수 result5 에 % 연산 후 출력
+	
+		
+		int v1 = 5;
+		int v2 = 2;
+		
+		int result1 = v1 + v2;
+		System.out.println(result1);
+		
+		int result2 = v1 - v2;
+		System.out.println(result2);
+		
+		int result3 = v1 * v2;
+		System.out.println(result3);
+	
+		int result4 = v1 / v2;
+		System.out.println(result4);
+		
+		int result5 = v1 % v2;
+		System.out.println(result5);
+	}
+
+}
--- src/com/myclass101/study/ch03/DenyLogicOperatorExample.java
+++ src/com/myclass101/study/ch03/DenyLogicOperatorExample.java
@@ -16,6 +16,14 @@ public class DenyLogicOperatorExample {
 		play = !play;
 		System.out.println("play: " + play);
 		
+		int j = 3;
+		
+		if(!(j==3)) {
+			System.out.println("3이 아님");
+		} else {
+			System.out.println("3임");
+		}
+		
 	}
 
 }
