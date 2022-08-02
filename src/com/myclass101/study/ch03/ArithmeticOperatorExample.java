e2a289190d157a4e473afb8cb84faff9dbf8ef6c
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-02 16:10:37 +0900

    up

--- src/com/myclass101/study/ch03/ArithmeticOperatorExample.java
+++ src/com/myclass101/study/ch03/ArithmeticOperatorExample.java
@@ -19,19 +19,19 @@ public class ArithmeticOperatorExample {
 		int v2 = 2;
 		
 		int result1 = v1 + v2;
-		System.out.println(result1);
+		System.out.println("result1: " +result1);
 		
 		int result2 = v1 - v2;
-		System.out.println(result2);
+		System.out.println("result2: " +result2);
 		
 		int result3 = v1 * v2;
-		System.out.println(result3);
+		System.out.println("result3: " +result3);
 	
 		int result4 = v1 / v2;
-		System.out.println(result4);
+		System.out.println("result4: " +result4);
 		
 		int result5 = v1 % v2;
-		System.out.println(result5);
+		System.out.println("result5: " +result5);
 	}
 
 }
