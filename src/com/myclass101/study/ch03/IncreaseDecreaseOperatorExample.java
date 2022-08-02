da97e852cdbc1f421ec1bd8b25c59e150d910314
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-02 15:15:15 +0900

    up

---
+++ src/com/myclass101/study/ch03/IncreaseDecreaseOperatorExample.java
@@ -0,0 +1,21 @@
+package com.myclass101.study.ch03;
+
+public class IncreaseDecreaseOperatorExample {
+
+	public static void main(String[] args) {
+
+//		++/-- 피연산자: 다른 연산 수행 전에 피연산자 1증가/ 감소
+//		피연산자 ++/--: 다른 연산 수행 후에 피연산자 1증가/ 감소
+		
+		int i = 0;
+		int j = 0;
+		
+		++i;
+		j++;
+		
+		System.out.println("i: " + i);
+		System.out.println("j: " + j);
+		
+	}
+
+}
