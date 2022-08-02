d9e3c07e4c6d05da03ae06550825c03f83edd8b2
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-02 15:57:14 +0900

    up

--- src/com/myclass101/study/ch03/DenyLogicOperatorExample.java
+++ src/com/myclass101/study/ch03/DenyLogicOperatorExample.java
@@ -4,6 +4,18 @@ public class DenyLogicOperatorExample {
 
 	public static void main(String[] args) {
 
+		boolean play = true;
+		
+		System.out.println("play: " + play);
+		
+//		논리부정연산자: !(느낌표)
+		
+		play = !play;
+		System.out.println("play: " + play);
+		
+		play = !play;
+		System.out.println("play: " + play);
+		
 	}
 
 }
