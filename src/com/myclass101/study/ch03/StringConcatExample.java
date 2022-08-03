f7eb73fb2880f5cb29d4106c3d1e177fd3ff6702
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-03 17:13:30 +0900

    up

---
+++ src/com/myclass101/study/ch03/StringConcatExample.java
@@ -0,0 +1,16 @@
+package com.myclass101.study.ch03;
+
+public class StringConcatExample {
+
+	public static void main(String[] args) {
+
+		String stringValue = "jdk";
+		String stringValue2 = "1.11";
+		
+		System.out.println("우리가 사용하는 " + stringValue + " " + stringValue2 + "입니다.");
+				
+		
+		
+	}
+
+}
