7168212b7a041845942eafcd2c4431642b0ad5bd
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-04 16:59:03 +0900

    up

---
+++ src/com/myclass101/study/ch03/ConditionalOperatorExample.java
@@ -0,0 +1,18 @@
+package com.myclass101.study.ch03;
+
+public class ConditionalOperatorExample {
+
+	public static void main(String[] args) {
+
+		int score = 85;
+		
+		char grade = score > 90 ? 'A' : 'B';
+		
+		System.out.println("grade: " + grade);
+		
+		
+		
+		
+	}
+
+}
