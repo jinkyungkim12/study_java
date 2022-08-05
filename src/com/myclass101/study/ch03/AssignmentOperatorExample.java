e441c7918524dca91b793a4f9597d315a7306d6b
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-05 11:22:04 +0900

    up

---
+++ src/com/myclass101/study/ch03/AssignmentOperatorExample.java
@@ -0,0 +1,32 @@
+package com.myclass101.study.ch03;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+
+		// +=
+		
+		int result = 0;
+		
+		result += 10;
+		// result = result + 10;
+		
+		System.out.println("result: " + result);
+		
+		
+		// -=
+		
+		int result2 = 0;
+		result2 -= 10;
+		// result2 = result2 - 10;
+		System.out.println("result2: " + result2);
+		
+		//*=
+		
+		int result3 = 0;
+		result3 *= 10;
+		// result3 = result3 * 10;
+		System.out.println("result3: " + result3);
+	}
+
+}
