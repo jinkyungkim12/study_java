f242d69cd98d375ac4e219890b9eed09e199ffc2
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-08 17:00:08 +0900

    up

---
+++ src/com/myclass101/study/ch04/IfExample.java
@@ -0,0 +1,16 @@
+package com.myclass101.study.ch04;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+
+		int score = 80;
+		
+		if(score >=90) {
+			System.out.println("A");
+			
+		}
+		
+	}
+
+}
