7886e83b9529f5b61872bad00476d121f4a9ea20
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-09 14:57:45 +0900

    up

---
+++ src/com/myclass101/study/ch04/IfExample3.java
@@ -0,0 +1,23 @@
+package com.myclass101.study.ch04;
+
+public class IfExample3 {
+
+	public static void main(String[] args) {
+
+		// int 타입의 변수를 2개 선언하고 
+		// 두 정수가 주어졌을 때 두 정수를 비교하고
+		// 크기 비교 결과를 나타내는 프로그램을 작성하시오
+		
+		int V1 = 10;
+		int V2 = 20;
+		
+		if(V2>V1){
+			System.out.println("V2가 V1보다 큽니다.");
+		}
+		else {
+			System.out.println("V1가 V2보다 큽니다.");
+		}
+		
+	}
+
+}
---
+++ src/com/myclass101/study/ch04/IfExample6.java
@@ -0,0 +1,10 @@
+package com.myclass101.study.ch04;
+
+public class IfExample6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
