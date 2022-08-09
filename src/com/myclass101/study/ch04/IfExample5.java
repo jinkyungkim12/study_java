2ec76fa409ab8753366e36a49b2b2f7d30b3559f
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-09 15:24:14 +0900

    up

--- src/com/myclass101/study/ch04/IfExample5.java
+++ src/com/myclass101/study/ch04/IfExample5.java
@@ -3,8 +3,28 @@ package com.myclass101.study.ch04;
 public class IfExample5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// int 타입 변수를 2개 선언하고
+		// 두 정수가 주어졌을 때 사분면 어디에 해당되는지 출력하는 프로그램
+		
+		int x = 10;
+		int y = -10;
+		
+		if (x>0 && y>0){ 
+			System.out.println("1사분면 입니다.");
+			
+		} else if(x<0 && y>0){
+			System.out.println("2사분면 입니다.");
+			
+		} else if(x<0 && y<0){
+			System.out.println("3사분면 입니다.");
+		
+		} else if(x>0 && y<0){
+			System.out.println("4사분면 입니다.");
+		
+		}else {
+			System.out.println("(0,0)입니다.");
+		
+		}
 	}
 
 }
