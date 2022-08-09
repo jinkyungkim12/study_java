be65a7266d454b43328f26220fca598a49d0f73b
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-09 16:13:35 +0900

    up

--- src/com/myclass101/study/ch04/IfExample2.java
+++ src/com/myclass101/study/ch04/IfExample2.java
@@ -6,26 +6,27 @@ public class IfExample2 {
 		
 		int grade = 1;
 		int price = 100000;
+		double sale6=0.0;
 		
 		if(grade == 1) {
 			int sale = (int) (price * 0.95);
-			System.out.println("일반 등급 할인율은 5%, 정가: " + price +" 원," + " 할인가 : " + sale +" 원 입니다.");
+			System.out.println("일반 등급 할인율은 5%, 정가: " + price +" 원," + " 할인가 : " + (int)sale +" 원 입니다.");
 		}
 		else if(grade == 2) {
 			int sale2 = (int) (price * 0.9);
-			System.out.println("브론즈 등급 할인율은 10%, 정가: " + price +" 원,"+ " 할인가 : " + sale2+" 원 입니다.");
+			System.out.println("브론즈 등급 할인율은 10%, 정가: " + price +" 원,"+ " 할인가 : " + (int)sale2+" 원 입니다.");
 		}
 		else if(grade == 3) {
 			int sale3 = (int) (price * 0.85);
-			System.out.println("실버 등급 할인율은 15%, 정가: " + price +" 원,"+ " 할인가 : " + sale3 +" 원 입니다.");
+			System.out.println("실버 등급 할인율은 15%, 정가: " + price +" 원,"+ " 할인가 : " + (int)sale3 +" 원 입니다.");
 		}
 		else if(grade == 4) {
 			int sale4 = (int) (price * 0.8);
-			System.out.println("골드 등급 할인율은 20%, 정가: " + price +" 원,"+ " 할인가 : " + sale4 +" 원 입니다.");
+			System.out.println("골드 등급 할인율은 20%, 정가: " + price +" 원,"+ " 할인가 : " + (int)sale4 +" 원 입니다.");
 		}
 		else if(grade == 5){
 			int sale5 = (int) (price * 0.973);
-			System.out.println("플래티넘 등급 할인율은 2.7%, 정가: " + price +" 원,"+ " 할인가 : " + sale5 +" 원 입니다.");
+			System.out.println("플래티넘 등급 할인율은 2.7%, 정가: " + price +" 원,"+ " 할인가 : " + (int)sale5 +" 원 입니다.");
 		}
 		else {
 			System.out.println("1~5 까지의 grade만 가능합니다.");
--- src/com/myclass101/study/ch04/IfExample4.java
+++ src/com/myclass101/study/ch04/IfExample4.java
@@ -3,6 +3,24 @@ package com.myclass101.study.ch04;
 public class IfExample4 {
 
 	public static void main(String[] args) {
+		// int 타입의 변수를 1개 선언하고 
+		// 짝수인지 홀수인지 판단하여 결과를 출력하는 프로그램을 작성하시오. 
+		
+		
+		int V1=4;
+		
+		if(V1 != 0) {
+			if(V1 % 2 == 0){
+				System.out.println("V1은 짝수입니다.");
+				
+			} else{
+				System.out.println("V1은 홀수입니다. ");
+				
+			}
+		} else {
+			 // V1==0
+			System.out.println("0은 홀수, 짝수를 판단할 수 없습니다. ");
+		}
 		
 		
 	}
