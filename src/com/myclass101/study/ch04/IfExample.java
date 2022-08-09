6a671e6ecee56f5c4d122a4fbbf0d8592606719f
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-09 13:59:37 +0900

    up

--- src/com/myclass101/study/ch04/IfExample.java
+++ src/com/myclass101/study/ch04/IfExample.java
@@ -15,9 +15,7 @@ public class IfExample {
 			} else {
 				System.out.println("A");
 			}
-		} 
-		
-		else if (score>=80 && score<90) {
+		} else if (score>=80 && score<90) {
 			if (score>=87){
 				System.out.println("B+");
 			} else if(score<=82){
@@ -25,9 +23,7 @@ public class IfExample {
 			} else {
 				System.out.println("B");
 			}
-		}
-		
-		else if (score>=70 && score<79) {
+		} else if (score>=70 && score<80) {
 			if (score>=77){
 				System.out.println("C+");
 			} else if(score<=72){
@@ -35,9 +31,7 @@ public class IfExample {
 			} else {
 				System.out.println("C");
 			}
-		}
-		
-		else if (score>=60 && score<69) {
+		} else if (score>=60 && score<70) {
 			if (score>=67){
 				System.out.println("D+");
 			} else if(score<=62){
@@ -45,9 +39,7 @@ public class IfExample {
 			} else {
 				System.out.println("D");
 			}
-		}
-		
-		else {
+		} else {
 			System.out.println("F");
 		}
 		
