a6b12971533747385119374ad43eaa9a90e3c123
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-10 15:18:59 +0900

    up

--- src/com/myclass101/study/ch04/MultiplicationTable.java
+++ src/com/myclass101/study/ch04/MultiplicationTable.java
@@ -18,6 +18,8 @@ public class MultiplicationTable {
 		for(int i=2; i<=9; i++) {
 			for(int j=1; j<=9; j++) {
 			System.out.println(i + " * " + j + " = " + (i*j) );
+			
+				if(j==9) System.out.println("---------------------");
 			}
 		}
 
--- src/com/myclass101/study/ch04/RandomSumExample.java
+++ src/com/myclass101/study/ch04/RandomSumExample.java
@@ -4,7 +4,7 @@ public class RandomSumExample {
 
 	public static void main(String[] args) {
 
-		
+		// 
 		
 	}
 
