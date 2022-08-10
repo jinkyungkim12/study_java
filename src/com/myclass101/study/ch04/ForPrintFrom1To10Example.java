2c6eef91655abb52dcf0a05ef8a926cffe8f9351
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-10 14:57:34 +0900

    up

--- src/com/myclass101/study/ch04/ForPrintFrom1To10Example.java
+++ src/com/myclass101/study/ch04/ForPrintFrom1To10Example.java
@@ -6,13 +6,25 @@ public class ForPrintFrom1To10Example {
 
 		// for문에는 i를 변수로 사용한다.
 		
-		int sum = 0;
+//		int sum = 0;
+//		
+//		for (int i=0; i<=10; i++) {
+//			System.out.println(i);
+//			sum = sum + i; // sum = sum + i -> sum += i;
+//		}
+//		System.out.println("sum: " + sum);
 		
-		for (int i=0; i<=10; i++) {
-			System.out.println(i);
-			sum = sum + i; // sum = sum + i -> sum += i;
-		}
-		System.out.println("sum: " + sum);
+		//1부터 10까지 더하는 프로그램 작성
+		
+			int sum = 0;
+			for(int i=0; i<=10; i++) {
+				System.out.println(i);
+				sum += i;
+				
+			}
+			
+			System.out.println("sum: " + sum);
+			
 	}
 
 }
---
+++ src/com/myclass101/study/ch04/MultiplicationTable.java
@@ -0,0 +1,17 @@
+package com.myclass101.study.ch04;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+
+		// 구구단 2단 출력 프로그램
+		// 2 * i = 2;
+		
+		for(int i=1; i<=9; i++) {
+		System.out.println("2 * " + i + " = " + (2 * i));
+			
+		}
+		
+	}
+
+}
