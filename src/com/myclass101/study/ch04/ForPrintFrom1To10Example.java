4be35ab7adff0ae3fe89faa65e9fd388c62a03a9
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-10 15:15:35 +0900

    up

--- src/com/myclass101/study/ch04/ForPrintFrom1To10Example.java
+++ src/com/myclass101/study/ch04/ForPrintFrom1To10Example.java
@@ -6,24 +6,22 @@ public class ForPrintFrom1To10Example {
 
 		// for문에는 i를 변수로 사용한다.
 		
-//		int sum = 0;
-//		
-//		for (int i=0; i<=10; i++) {
-//			System.out.println(i);
-//			sum = sum + i; // sum = sum + i -> sum += i;
-//		}
-//		System.out.println("sum: " + sum);
+		int sum = 0;
+		
+		for (int i=0; i<=10; i++) {
+			System.out.println(i);
+			sum = sum + i; // sum = sum + i -> sum += i;
+		}
+	System.out.println("sum: " + sum);
 		
 		//1부터 10까지 더하는 프로그램 작성
 		
-			int sum = 0;
-			for(int i=0; i<=10; i++) {
-				System.out.println(i);
-				sum += i;
-				
-			}
-			
-			System.out.println("sum: " + sum);
+			//int sum = 0;
+			//for(int i=0; i<=10; i++) {
+			//	System.out.println(i);
+			//	sum += i;
+			//}
+			//System.out.println("sum: " + sum);
 			
 	}
 
--- src/com/myclass101/study/ch04/MultiplicationTable.java
+++ src/com/myclass101/study/ch04/MultiplicationTable.java
@@ -12,6 +12,13 @@ public class MultiplicationTable {
 			
 		}
 		
-	}
+		// 2단 ~ 9 단 까지 출력하는 프로그램 작성: 6줄 이상 되지 않	게
+		
+		for(int i=2; i<=9; i++) {
+			for(int j=1; j<=9; j++) {
+			System.out.println(i + " * " + j + " = " + (i*j) );
+			}
+		}
 
+	}
 }
