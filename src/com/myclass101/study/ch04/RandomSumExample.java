809be15492b02a3bcde05c4889d852887c6611cf
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-10 17:22:18 +0900

    up

--- src/com/myclass101/study/ch04/RandomSumExample.java
+++ src/com/myclass101/study/ch04/RandomSumExample.java
@@ -11,20 +11,28 @@ public class RandomSumExample {
 		//int random = (int) ((Math.random() * (max - min)) + min);
 		//System.out.println(random);
 		
-		double min = 1;
-		double max = 100;
+		double min = 1.0;
+		double max = 100.0;
 		int random = (int) ((Math.random() * (max - min)) + min);
 		int random2 = (int) ((Math.random() * (max - min)) + min);
 		int sum=0;
 		
-		for(int i=random+1; i<random2; i++) {
-		sum += i;
-		} if(random2>random){
+		if(random2>random){
+			for(int i=random+1; i<random2; i++) {
+				sum += i;
+			} 
 			System.out.println("random: "+ random + " random2: " + random2 + " sum = " + sum);
+			
+		} else if(random>random2){
+			for(int i=random2+1; i<random; i++) {
+				sum += i;
+			}
+			System.out.println("random: "+ random + " random2: " + random2 + " sum = " + sum);
+			
 		} else {
-			System.out.println("다시 돌리세요.");
+			System.out.println("두 수 사이의 수가 존재하지 않습니다.");
 		}
 		
-	}
+	}  
 
 }
