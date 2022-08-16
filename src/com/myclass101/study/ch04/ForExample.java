c22c264d0bdc18eb4ef887154b01549b9ce690d4
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-16 17:26:27 +0900

    up

---
+++ src/com/myclass101/study/ch04/ForExample.java
@@ -0,0 +1,95 @@
+package com.myclass101.study.ch04;
+
+public class ForExample {
+
+	public static void main(String[] args) {
+
+		
+	//		**********
+	//		**********
+	//		**********
+	//		**********
+	//		**********
+	//		출력하는 프로그램을 만드시요
+			
+			
+	//		**********
+	//		*        *
+	//		*        *
+	//		*        *
+	//		**********
+	//		출력하는 프로그램을 만드시요
+			
+			
+	//		1
+	//		12
+	//		123
+	//		1234
+	//		12345
+	//		123456
+	//		1234567
+	//		12345678
+	//		123456789
+	//		1 3 5 7 90
+	//		출력하는 프로그램을 만드시요
+
+
+		
+		//1번 문제
+		
+		for(int i=1; i<=9; i++) {
+			for(int j=1; j<=9; j++){
+			System.out.print("*");
+		}
+		System.out.println("*");
+		 if(i==5) {
+			 break;
+		 }
+		}
+		
+		System.out.println("------------------");
+
+		
+		//2번 문제
+		
+		String star2 = "**********";
+		String star3 = "*        *";
+		System.out.println(star2);
+		System.out.println(star3);
+		System.out.println(star3);
+		System.out.println(star3);
+		System.out.println(star2);
+		
+		System.out.println("------------------");
+		
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=10; j++){
+			if(i>1 && i<5 && j>1 && j<=9){
+				System.out.print(" ");
+			}else {
+				System.out.print("*");
+			}
+		}
+		System.out.println(" ");
+		}
+		
+		System.out.println("------------------");
+		
+		// 3번 문제
+				
+		for(int i=1; i<=10; i++) {
+			for(int j=1; j<=i; j++) {
+				if(i==10 && j % 2 == 0){
+					if(j==10) {
+						System.out.println("0");
+					}
+					System.out.print(" ");
+				} else {
+					System.out.print(j);
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
