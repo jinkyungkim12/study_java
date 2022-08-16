5978e7f5b8b2098ad9d30fe34e4df3c13a3bfb00
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-16 14:35:15 +0900

    up

--- src/com/myclass101/study/ch04/RandomSumExample2.java
+++ src/com/myclass101/study/ch04/RandomSumExample2.java
@@ -6,16 +6,7 @@ public class RandomSumExample2 {
 
 		//1~100 사이의 랜덤 정수 2개를 발생시켜서 두 수 사이의 합을 구하라
 		
-		//double min = 1.0;
-		//double max = 100.0;
-		//int random = (int) ((Math.random() * (max - min)) + min);
-		//System.out.println(random);
-		
-		
-		
-		// 1 ~ 100 사이의 랜덤 정수 2개를 발생시켜
-				// 두 수 사이의 합을 구하라
-				
+						
 				double num1 = 1.0;
 				double num2 = 100.0;
 				
