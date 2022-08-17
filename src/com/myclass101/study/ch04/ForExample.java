36af7d877018a2c02ed0fdba48f23c021933921d
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-17 15:15:48 +0900

    up

--- src/com/myclass101/study/ch04/ForExample.java
+++ src/com/myclass101/study/ch04/ForExample.java
@@ -52,16 +52,7 @@ public class ForExample {
 		
 		//2번 문제
 		
-		String star2 = "**********";
-		String star3 = "*        *";
-		System.out.println(star2);
-		System.out.println(star3);
-		System.out.println(star3);
-		System.out.println(star3);
-		System.out.println(star2);
-		
-		System.out.println("------------------");
-		
+				
 		for(int i=1; i<=5; i++) {
 			for(int j=1; j<=10; j++){
 			if(i>1 && i<5 && j>1 && j<=9){
@@ -89,6 +80,26 @@ public class ForExample {
 				}
 			}
 			System.out.println();
+		}
+		
+		System.out.println("------------------");
+		
+		for(int i=1; i<=10; i++) {
+			if(i==10) {
+				for(int j=1; j<=i-1; j++) {
+					if(j%2==1) {
+						System.out.print(j);
+					} else {
+						System.out.print(" ");
+					}
+				}
+				System.out.println("0");
+			} else {
+				for(int j=1; j<=i; j++) {
+					System.out.print(j);
+				}
+				System.out.println();
+			}
 		}
 	}
 
