a77ef558f399594df584922ce7e210e25003c6f2
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-18 16:35:53 +0900

    up

--- src/com/myclass101/study/ch05/EnumWeekExample.java
+++ src/com/myclass101/study/ch05/EnumWeekExample.java
@@ -3,8 +3,13 @@ package com.myclass101.study.ch05;
 public class EnumWeekExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Week abc;
+		
+
+		
+		System.out.println("Week.FRIDAY: " + Week.FRIDAY);
+		// System.out.println("abc: " + abc);
 	}
 
 }
