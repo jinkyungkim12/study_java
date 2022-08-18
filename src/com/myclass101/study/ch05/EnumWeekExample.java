869f05db8f8806cd6fc858b5f1ede52ef7b63e32
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-18 17:08:40 +0900

    up

--- src/com/myclass101/study/ch05/EnumWeekExample.java
+++ src/com/myclass101/study/ch05/EnumWeekExample.java
@@ -6,10 +6,38 @@ public class EnumWeekExample {
 
 		Week abc;
 		
-
+		Week xyz;
+		
+		abc= Week.MONDAY;
 		
 		System.out.println("Week.FRIDAY: " + Week.FRIDAY);
-		// System.out.println("abc: " + abc);
+		System.out.println("abc: " + abc);
+		
+		
+		//String name = abc;
+		String name = abc.name();
+		System.out.println("name: " + name);
+		
+		int ordinal = abc.ordinal();
+		System.out.println("ordinal: " + ordinal);
+		
+		
+		xyz = Week.THURSDAY;
+		
+		int result1 = abc.compareTo(xyz);
+		int result2 = xyz.compareTo(abc);
+		
+		System.out.println("result1: " + result1);
+		System.out.println("result2: " + result2);
+		
+		
+		Week[] days = Week.values();
+		
+		int index = 0;
+		for(Week day: days) {
+			System.out.println(index + ": day: " + day);
+			index ++;
+		}
 	}
 
 }
