47dc1916cc0fa0dc5648d8609a1bb8c581d9968d
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-22 16:08:14 +0900

    up

--- src/com/myclass101/study/ch06/CarExample.java
+++ src/com/myclass101/study/ch06/CarExample.java
@@ -5,13 +5,19 @@ public class CarExample {
 	public static void main(String[] args) {
 
 		
-		Car abc = new Car();
+		Car car = new Car();
+		Car abx = new Car();
+		
+		
 		int a = 1;
+		int b = 1;
 		
-		int[] b = new int[1];
+		int[] c = new int[1];
 		
-		System.out.println("car.name: " + abc.name);
-		System.out.println("car.name2[1]: " + abc.name2[1]);
+		System.out.println("car.name: " + car.name);
+		System.out.println("car.name2[1]: " + car.name2[1]);
+		System.out.println("abx.name: " + abx.name);
+		System.out.println("abx.name2[1]: " + abx.name2[1]);
 		
 	}
 
