9a13d5e98ac0da70df50a05fa23b0e39a522f8ea
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-22 16:02:21 +0900

    up

--- src/com/myclass101/study/ch06/CarExample.java
+++ src/com/myclass101/study/ch06/CarExample.java
@@ -11,7 +11,7 @@ public class CarExample {
 		int[] b = new int[1];
 		
 		System.out.println("car.name: " + abc.name);
-		System.out.println("car.name2[1]: " + abc.name2);
+		System.out.println("car.name2[1]: " + abc.name2[1]);
 		
 	}
 
