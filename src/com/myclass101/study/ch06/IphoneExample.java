ee204d451d40398bd3f603a1cbce771b8cb36871
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-22 16:44:08 +0900

    up

--- src/com/myclass101/study/ch06/IphoneExample.java
+++ src/com/myclass101/study/ch06/IphoneExample.java
@@ -10,18 +10,27 @@ public class IphoneExample {
 		System.out.println("iphone.name: " + iphone.name);
 		System.out.println("현재 나의 아이폰: " + iphone.name2[2]);
 		
+		System.out.println("----------------------------");
+		
+		//함수
 		iphone.pr();
 		
 		System.out.println("----------------------------");
 		
 		Car car = new Car();
-		
 		System.out.println("car.name: " + car.name);
 		
+		System.out.println("----------------------------");
 		
 		Handbag handbag = new Handbag();
+		System.out.println("handbag.name: " + handbag.name);
 		
-		System.out.println("handbag.name: " +handbag.name);
+		System.out.println("----------------------------");
+		
+		car.name = "소나타";
+		System.out.println("car.name: " + car.name);
+
+		
 	}
 
 }
