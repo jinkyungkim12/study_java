932bf120dc04f8d671a5d918d3fc78b665d44f3a
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-22 15:59:30 +0900

    up

---
+++ src/com/myclass101/study/ch06/Car.java
@@ -0,0 +1,16 @@
+package com.myclass101.study.ch06;
+
+public class Car {
+
+	// filed : 변수
+	
+	int a = 1;
+	String name = "장원영";
+	String[] name2 = {"장원영", "고소영"};
+	
+	
+	
+	// method : 함수
+	
+	
+}
---
+++ src/com/myclass101/study/ch06/CarExample.java
@@ -0,0 +1,18 @@
+package com.myclass101.study.ch06;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+
+		
+		Car abc = new Car();
+		int a = 1;
+		
+		int[] b = new int[1];
+		
+		System.out.println("car.name: " + abc.name);
+		System.out.println("car.name2[1]: " + abc.name2);
+		
+	}
+
+}
