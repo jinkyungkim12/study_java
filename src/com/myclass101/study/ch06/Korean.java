5e5dbc99a4c6aef3090a9fea9f2be97c54e671d2
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-23 17:37:22 +0900

    up

--- src/com/myclass101/study/ch06/Korean.java
+++ src/com/myclass101/study/ch06/Korean.java
@@ -2,25 +2,49 @@ package com.myclass101.study.ch06;
 
 public class Korean {
 
-	String name = "김진경";
-	String ssn = "123456-123456";
-	int age = 30;
+//	String name = "김진경";
+//	String ssn = "123456-123456";
+//	int age = 30;
+//	
+//	
+//	Korean(){
+//		
+//	}
+//	
+//	Korean(String name, String ssn){
+//		
+//		this.name = name;
+//		this.ssn = ssn;
+//	}
+//	
+//	Korean(String name, int age){
+//			
+//		this.name = name;
+//		this.age = age;
 	
+	
+	
+	
+	String name = "";
+	String ssn = "";
+	int age = 1;
 	
 	Korean(){
 		
 	}
 	
 	Korean(String name, String ssn){
-		
 		this.name = name;
 		this.ssn = ssn;
-	}
+		}
 	
 	Korean(String name, int age){
-			
 		this.name = name;
 		this.age = age;
 	}
 	
+
+	
 }
+	
+
--- src/com/myclass101/study/ch06/KoreanExample.java
+++ src/com/myclass101/study/ch06/KoreanExample.java
@@ -4,14 +4,29 @@ public class KoreanExample {
 
 	public static void main(String[] args) {
 
-		Korean k1 = new Korean("차은우", "970303-123456");
-		System.out.println("k1.name: " + k1.name);
-		System.out.println("k1.ssn: " + k1.ssn);
+//		Korean k1 = new Korean("차은우", "970303-123456");
+//		System.out.println("k1.name: " + k1.name);
+//		System.out.println("k1.ssn: " + k1.ssn);
+//		
+//		
+//		Korean k2 = new Korean("김진경", 30);
+//		System.out.println("k2.name: " + k2.name);
+//		System.out.println("k2.age: " + k2.age);
 		
 		
-		Korean k2 = new Korean("김진경", 30);
-		System.out.println("k2.name: " + k2.name);
-		System.out.println("k2.age: " + k2.age);
+		
+		
+		
+		Korean K1 = new Korean("차은우", "970507-123456");
+		System.out.println("name: " + K1.name);
+		System.out.println("ssn: " + K1.ssn);
+
+		
+		Korean K2 = new Korean("김진경", 30);
+		System.out.println("name: " + K2.name);
+		System.out.println("age: " + K2.age);
+		
+		
 	}
 
 }
