f5b94c7bbd718b89f0dd09dc60c42aaa1cdf3a19
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-24 16:10:04 +0900

    up

---
+++ src/com/myclass101/study/ch06/Television.java
@@ -0,0 +1,16 @@
+package com.myclass101.study.ch06;
+
+public class Television {
+
+	
+		public static String company = "Samsung";
+		public static String model = "OLED";
+		public static String info;
+		
+		
+		public String version = "1.0";
+		
+		static {
+			info = company + " " + model;
+		}
+}
---
+++ src/com/myclass101/study/ch06/TelevisionExample.java
@@ -0,0 +1,13 @@
+package com.myclass101.study.ch06;
+
+public class TelevisionExample {
+
+	public static void main(String[] args) {
+
+		System.out.println(Television.company);
+		System.out.println(Television.model);
+		System.out.println(Television.info);
+		
+	}
+
+}
