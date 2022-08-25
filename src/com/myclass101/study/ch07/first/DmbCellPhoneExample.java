7ae7b7f85b0b51bacd694acc523d142765f884e3
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-25 16:16:19 +0900

    up

--- src/com/myclass101/study/ch07/first/DmbCellPhoneExample.java
+++ src/com/myclass101/study/ch07/first/DmbCellPhoneExample.java
@@ -4,33 +4,45 @@ public class DmbCellPhoneExample {
 
 	public static void main(String[] args) {
 
+		// 부모클래스 생성, 함수 호출
 
-		// 부모 객체 생성하고 poweron 메서드 실횅
+		CellPhone cellPhone = new CellPhone();
+		
+		cellPhone.powerOn();
+		cellPhone.powerOff();
+		cellPhone.bell();
+		cellPhone.sendVoice("안녕");
+		cellPhone.receiveVoice("하이");
+		cellPhone.hangUp();
+		
+		System.out.println("---------------------");
+		
+		// 자식클래스 생성, 함수 호출
+		
+		DmbCellPhone dmbCellPhone = new DmbCellPhone();
+		
+		dmbCellPhone.turnOnDmb();
+		dmbCellPhone.changeChannelDmb(12);
+		dmbCellPhone.turnOffDmb();
+		
+		System.out.println("---------------------");
+		
+		// 자식클래스 생성, 부모자식 함수 호출
+		
+		DmbCellPhone dmbCellPhone2 = new DmbCellPhone();
+		
+		dmbCellPhone2.powerOn();
+		dmbCellPhone2.powerOff();
+		dmbCellPhone.bell();
+		dmbCellPhone2.sendVoice("부모");
+		dmbCellPhone2.receiveVoice("자식");
+		dmbCellPhone2.hangUp();
+		
+		dmbCellPhone2.turnOnDmb();
+		dmbCellPhone2.changeChannelDmb(7);
+		dmbCellPhone2.turnOffDmb();
 		
 		
-		CellPhone cellphone = new CellPhone();
-		
-		cellphone.powerOn();
-		System.out.println("-------------------------");
-		
-		// 자식 객체 생성
-		
-		DmbCellPhone dmbcellphone = new DmbCellPhone();
-		
-		// 자식 객체의 turnOffDmb 실횅
-		
-		dmbcellphone.turnOffDmb();
-		System.out.println("-------------------------");
-		
-		// 생성자를 이용하여 자식 객체 생성
-		
-		DmbCellPhone dmbcellphone2 = new DmbCellPhone("iphone12mini", "white", 12);
-		
-		// dmbcellphone2 객체를 사용하여 자식 클래스의 함수 전체 호출
-		
-		dmbcellphone2.turnOnDmb();
-		dmbcellphone2.changeChannelDmb(7);
-		dmbcellphone2.turnOffDmb();
 		
 	}
 
