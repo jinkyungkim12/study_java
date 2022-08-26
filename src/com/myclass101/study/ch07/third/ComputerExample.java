ec77b4f0a2e0babe863e87c8361b65d258dc9c9f
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-26 17:00:42 +0900

    up

--- src/com/myclass101/study/ch07/third/ComputerExample.java
+++ src/com/myclass101/study/ch07/third/ComputerExample.java
@@ -3,8 +3,20 @@ package com.myclass101.study.ch07.third;
 public class ComputerExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int r = 10;
+		
+		Calculator calculator = new Calculator();
+		
+		System.out.println("원면적: " + calculator.areaCircle(2));
+		
+		
+		
+		Computer computer = new Computer();
+		
+		System.out.println("원면적: " + computer.areaCircle(2));
+		
+		
 	}
 
 }
