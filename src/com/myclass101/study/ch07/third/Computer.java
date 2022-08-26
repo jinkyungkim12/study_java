71e00c8fff668d075819dd43e249cc84313974ea
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-26 16:24:18 +0900

    up

---
+++ src/com/myclass101/study/ch07/third/Computer.java
@@ -0,0 +1,13 @@
+package com.myclass101.study.ch07.third;
+
+public class Computer extends Calculator{
+	
+	@Override
+	public double areaCircle(double r) {
+		System.out.println("Calculator 객체의 areaCircle 실행");
+		
+		return Math.PI * r * r;
+
+	}
+	
+}
