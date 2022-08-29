5d41759927e841c48f887e2c9fd9e993a7e4e581
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-29 18:05:57 +0900

    up

---
+++ src/com/myclass101/study/codingTeam/Aug/fifth/For1to10_20220829.java
@@ -0,0 +1,17 @@
+package com.myclass101.study.codingTeam.Aug.fifth;
+
+public class For1to10_20220829 {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+		
+		for(int i = 1; i<=10; i++) {
+			sum += i;
+		}
+		
+		System.out.println("result: " + sum);
+		
+	}
+
+}
