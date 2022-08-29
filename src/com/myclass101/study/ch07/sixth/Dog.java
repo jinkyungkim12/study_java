b02997c6f1368afeae932422e5d704b636bc50bb
jinkyungkim12 <skwlsrud3@naver.com>
2022-08-29 17:08:35 +0900

    up

--- src/com/myclass101/study/ch07/sixth/Dog.java
+++ src/com/myclass101/study/ch07/sixth/Dog.java
@@ -1,5 +1,13 @@
 package com.myclass101.study.ch07.sixth;
 
-public class Dog {
+public class Dog extends Animal {
 
+	public Dog() {
+		this.kind = "포유류";
+	}
+	
+	@Override
+	public void sound() {
+		System.out.println("으르렁");
+	}
 }
